f379b852839b08ed3f5bda02ae2fa2054ab9e3bf
Fuadtag <Fuadjt@code.edu.az>
2019-07-14 12:38:49 +0400

    Task 2 done

---
+++ js/main.js
@@ -0,0 +1,18 @@
+"use strict"
+let result = 0;
+
+function task2(num1, num2) {
+    if (num2 > num1) {
+        for (let i = num1; i <= num2; i++) {
+            if (i % 2 == 0) {
+                if (i <= num2) {
+                    result += i;
+                }
+
+
+            }
+        }
+        return result;
+    } else(alert(disable))
+}
+alert(task2(10, 78))
